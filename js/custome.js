25e943ccb5044fb0aa3fe81da2e35b8424a90483
Abdallah Saqer <peposaqer@outlook.com>
2021-04-03 15:50:06 +0200

    first commit

---
+++ js/custome.js
@@ -0,0 +1,58 @@
+/* global */
+
+$(function () {
+    'use strict';
+    // trigger
+    $('html').niceScroll({ cursorcolor: '#f7600e' });
+    //chang header hight
+    $('.header').height($(window).height());
+    // scrol to features
+    $('.header .arrow i').click(function () {
+        $('html, body').animate({
+            scrollTop: $('.features').offset().top
+        }, 1000);
+    });
+    //shw hidden
+    $('.showmore').click(function () {
+        $('.our-works .hidden').removeClass('hidden').fadeIn();
+        $(".our-works .showmore").addClass("hidden");
+    });
+    // $(".showless").click(function () {
+    //     $(".our-works .hidden").addClass("hidden").fadeIn();
+    //     $(".our-works .showless").addClass("hidden");
+    //     $(".our-works .showmore").addClass("hidden");
+    // });
+    // check testin
+    var leftArrow = $('.testin .fa-chevron-left'),
+        rightArrow = $('.testin .fa-chevron-right');
+    function checkClients() {
+        $('.client:first').hasClass('active') ? leftArrow.fadeOut() : leftArrow.fadeIn();
+        $('.client:last').hasClass('active') ? rightArrow.fadeOut() : rightArrow.fadeIn();
+    }
+    checkClients();
+    $('.testin i').click(function () {
+
+        if ($(this).hasClass('fa-chevron-right')) {
+
+            $('.testin .active').fadeOut(100, function () {
+
+                $(this).removeClass('active').next('.client').addClass('active').fadeIn();
+            
+                checkClients();
+            
+            });
+
+        } else {
+
+            $('.testin .active').fadeOut(100, function () {
+
+                $(this).removeClass('active').prev('.client').addClass('active').fadeIn();
+            
+            });
+            
+                checkClients();
+            
+        }
+
+    });
+}); 
